d774b519865273fad2588989408ccec2e9832280
Greg Lindahl <lindahl@pbm.com>
2021-11-29 21:43:30 +0000

    working client demo

--- influxdb2.py
+++ influxdb2.py
@@ -1,24 +1,32 @@
 import time
 
-import influxdb_client
 from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import ASYNCHRONOUS
+from influxdb_client.client.write_api import ASYNCHRONOUS, SYNCHRONOUS
 from influxdb_client.client.exceptions import InfluxDBError
 from influxdb_client.domain.write_precision import WritePrecision
 
 client = InfluxDBClient.from_config_file('influxdb2_config.ini')
+# does file exist
+# does it have url, org, token
+# any '#' or whitespace
+# might need to return the org ?
+
 bucket = 'vlbimon'
 
-write_api = client.write_api(write_options=ASYNCHRONOUS)
+write_api = client.write_api(write_options=SYNCHRONOUS)  # SYNCHRONOUS or ASYNCHRONOUS
 
-t = time.time()
+t = int(time.time())
 
-p = Point('my_measurement').tag('location', 'Prague').field('temperature', 25.3).time(int(t), write_precision=WritePrecision.S)
+p = Point('my_measurement').tag('location', 'Prague').field('temperature', 25.3).time(t, write_precision=WritePrecision.S)
 
-async_result = write_api.write(bucket=bucket, record=p)
+try:
+    async_result = write_api.write(bucket=bucket, record=p)
+except InfluxDBError:
+    # I have never seen this happen XXX try stopping the influx instance mid-write
+    pass
 
-print('type', type(async_result))  # type <class 'multiprocessing.pool.ApplyResult'>
+if async_result is not None:
+    print('type', type(async_result))  # type <class 'multiprocessing.pool.ApplyResult'>
+    print(async_result.get())  # None
 
-print(async_result.get())  # None
-
-client.close()  # required or hang on exit
+client.close()  # required (for async) or hang on exit
